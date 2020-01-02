1e9f8ee588a417e26668d893f32395193b4aad41
tda1312 <tda1312@gmail.com>
2020-01-02 12:13:00 +0700

    Save current state

--- huffman.c
+++ huffman.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+// function to find fibonacci number
+int find_fibo(int n){
+    if(n <= 1){
+        return n;
+    }
+    return find_fibo(n - 1) + find_fibo(n - 2);
+}
+
 int main(){
     int i, j;
     char filename[] = "lena_gray.bmp";
@@ -8,9 +16,10 @@ int main(){
     long imgsize, imgdataoff;
     int** image;
 
+    // read input image file
     FILE* image_file;
-
     image_file = fopen(filename, "rb");
+
     if(image_file == NULL){
         printf("Error opening file! Please try again.");
         exit(1);
@@ -54,6 +63,50 @@ int main(){
         // number of bytes in the Image array
         int num_bytes = (imgsize - imgdataoff) / 3;
 
-        
+        // read Image file into Image array
+        for(i = 0; i < height; i++){
+            for(j = 0; j < width; j++){
+                fread(&temp, 3, 1, image_file);
+
+                // this case the Image is a 24-bit BMP Image
+                temp = temp & 0x0000FF;
+                image[i][j] = temp;
+            }
+        }
     }
+
+    // calculate histogram
+    int histogram[256];
+    for(i = 0; i < 256; i++){
+        histogram[i] = 0;
+    }
+    for(i = 0; i < height; i++){
+        for(j = 0; j < width; i++){
+            histogram[image[i][j]] += 1;
+        }
+    }
+
+    // find non-zero occurences
+    int nodes = 0;
+    for(i = 0; i < 256; i ++){
+        if(histogram[i] != 0){
+            nodes += 1;
+        }
+    }
+
+    // find minimum probability
+    float p = 1.0, ptemp;
+    for(i = 0; i < 256; i ++){
+        ptemp = (histogram[i] / (float)(height * width));
+        if(ptemp > 0 && ptemp <= p){
+            p = ptemp;
+        }
+    }
+
+    // calculate max length of codeword
+    i = 0;
+    while((1 / p) > find_fibo(i)){
+        i++;
+    }
+    int max_codelen = i - 3;
 }
